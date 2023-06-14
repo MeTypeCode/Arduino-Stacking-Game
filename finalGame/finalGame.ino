b45281d377cb5bfcba261ba9a93a613a228429ff
Kylan <Kyrileyj@gmail.com>
2023-06-13 21:09:23 -0700

    added sound

--- finalGame/finalGame.ino
+++ finalGame/finalGame.ino
@@ -1,18 +1,20 @@
 #include <AsyncDelay.h>
 #include <Adafruit_Circuit_Playground.h>
-// #include <string>
-// using namespace std;
 
+//timers
 AsyncDelay lightTimer;
 AsyncDelay toneTimer;
 
+//switch
 int switchPin = 7;
 volatile bool switchFlag = 0;
 bool switchState = 0;
 
+//right button
 int buttonBPin = 5;
 volatile bool buttonBFlag = 0;
 
+//left button
 int buttonAPin = 4;
 volatile bool buttonAFlag = 0;
 
@@ -29,7 +31,9 @@ bool hasRightStacked = 0;
 int leftCurrentStack = 5;
 int rightCurrentStack = 4;
 
-int gameOver = 0;
+bool gameOver = 0;
+bool victory = 0; 
+bool secondTone = 0;
 
 //for notes
 float midi[127];
@@ -57,6 +61,7 @@ void setup() {
 
   //timers setup
   lightTimer.start(1000, AsyncDelay::MILLIS);
+  toneTimer.start(1000, AsyncDelay::MILLIS);
 
   //set first light
   CircuitPlayground.setPixelColor(currentLed, 0xFF00FF);
@@ -66,37 +71,67 @@ void setup() {
 void loop() {
   // put your main code here, to run repeatedly:
   if (!gameOver) {
+    //check switch
     if (switchFlag) {
-      delay(5);
+      delay(5); //debounce
       switchState = digitalRead(switchPin);
 
       switchFlag = 0;
     }
 
+    //when right button pressed
     if (buttonAFlag) {
-      delay(5); //might need to make higher, didn't work earlier
-      if (!hasLeftStacked) {
-        if (currentLed == leftCurrentStack) {
+      delay(5); //debounce
+      if (!hasLeftStacked) {  //make sure you can only add one light to a side per cycle
+        if (currentLed == leftCurrentStack) { //check if current lit up led is the correct one
           hasLeftStacked = 1;
           leftCurrentStack += 1;
           stackColors[currentLed] = levelColors[level];
+          
+          if (switchState) { // play sound if switch is on
+            noTone(CPLAY_BUZZER);
+            tone(CPLAY_BUZZER, midi[60]);
+            toneTimer.start(100, AsyncDelay::MILLIS);
+            secondTone = 1;
+          }
         } else {
-          gameOver = 1;
+          gameOver = 1;  //if it is the wrong light end the game
+          if (switchState) {
+            CircuitPlayground.playTone(midi[50], 300);
+            CircuitPlayground.playTone(midi[48], 300);
+            CircuitPlayground.playTone(midi[46], 300);
+            CircuitPlayground.playTone(midi[42], 500);
+          }
         }
       }
 
       buttonAFlag = 0;
     } 
 
+
+    //when left button pressed
     if (buttonBFlag) {
-      delay(5); //might need to make higher, didn't work earlier
-      if (!hasRightStacked) {
-        if (currentLed == rightCurrentStack) {
+      delay(5); //debounce
+      if (!hasRightStacked) {  //make sure you can only add one light to a side per cycle
+        if (currentLed == rightCurrentStack) { //check if current lit up led is the correct one
           hasRightStacked = 1;
           rightCurrentStack += -1;
           stackColors[currentLed] = levelColors[level];
+
+          if (switchState) { // play sound if switch is on
+            noTone(CPLAY_BUZZER);
+            tone(CPLAY_BUZZER, midi[60]);
+            toneTimer.start(100, AsyncDelay::MILLIS);
+            secondTone = 1;
+          }
         } else {
-          gameOver = 1;
+          gameOver = 1; //if it is the wrong light end the game
+          if (switchState) {
+            CircuitPlayground.playTone(midi[50], 300);
+            CircuitPlayground.playTone(midi[48], 300);
+            CircuitPlayground.playTone(midi[46], 300);
+            CircuitPlayground.playTone(midi[42], 500);
+          }
         }
       }
       
@@ -104,14 +139,16 @@ void loop() {
       buttonBFlag = 0;
     }
 
-    if (lightTimer.isExpired()) {
-      CircuitPlayground.setPixelColor(currentLed, stackColors[currentLed]);
-      if ((currentLed == 9 && bounceDirection == 1) || (currentLed == 0 && bounceDirection == -1)) {
+
+    //light pattern
+    if (lightTimer.isExpired()) {   
+      CircuitPlayground.setPixelColor(currentLed, stackColors[currentLed]);  //turn the current pixel back to its orignial color
+      if ((currentLed == 9 && bounceDirection == 1) || (currentLed == 0 && bounceDirection == -1)) {  //change the direction the light moves if at one of the edges
         bounceDirection *= -1;
       } 
 
-      currentLed += bounceDirection;
-      CircuitPlayground.setPixelColor(currentLed, 0xFF00FF);
+      currentLed += bounceDirection;  //increment to next led
+      CircuitPlayground.setPixelColor(currentLed, 0xFF00FF);  //set new current led to purple
 
       //reset has stacked for each side
       if (currentLed == 4 && bounceDirection == 1) {
@@ -120,15 +157,41 @@ void loop() {
         hasRightStacked = 0;
       } 
 
+      //if the stack is full increase the level by 1 and reset the stacks
       if (rightCurrentStack == -1 && leftCurrentStack == 10) {
         level += 1;
         rightCurrentStack = 4;
         leftCurrentStack = 5;      
       }
 
-      lightTimer.start(240 - level * 20, AsyncDelay::MILLIS);
+      lightTimer.start(240 - level * 20, AsyncDelay::MILLIS); //start delay for next light pattern cycle, time gets faster as level increases
     }
 
+    //stop tones from playing if they have lasted their duration
+    if (toneTimer.isExpired()) {
+      noTone(CPLAY_BUZZER);
+
+      if (secondTone) {
+        tone(CPLAY_BUZZER, midi[70]);
+        toneTimer.start(200, AsyncDelay::MILLIS);  
+        secondTone = 0;      
+      }
+    }
+
+    //check if won
+    if (level == 3) {
+      victory = 1;
+      gameOver = 1;
+    }
+    
+
+  } else if (victory) { //play victory melody
+    if (switchState) {
+      CircuitPlayground.playTone(midi[60], 500);
+      CircuitPlayground.playTone(midi[64], 500);
+      CircuitPlayground.playTone(midi[67], 500);
+      CircuitPlayground.playTone(midi[72], 500);
+    }
   }
 }
 
